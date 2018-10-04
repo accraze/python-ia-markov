1a77fff3d6a1a46bfb6a4748d2b3350f10ecc209
ex00 <ex00@users.noreply.github.com>
2018-10-04 15:53:09 +0400

    sort imports

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ from os.path import basename
 from os.path import dirname
 from os.path import join
 from os.path import splitext
+
 from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.install import install
